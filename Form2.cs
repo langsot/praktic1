27ef58837898487b187c15e96e428c1b7b9ca24f
langsot <arashi_hyuga@mail.ru>
2017-02-24 23:29:44 +0700

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form2 : Form
+    {
+
+        //модальное окно - это когда нельзя перейти к 1 форме не завершив вторую
+        public string request
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            Close();
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+          
+        }       
+    }
+}
